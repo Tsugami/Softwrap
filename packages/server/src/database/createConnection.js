3dd85b83616bcc8bf3ac2c62c4a63c8f591f2e08
Yslan Ramos <yslan3000@gmail.com>
2020-12-09 11:44:24 -0300

    fix: remove credentials

--- packages/server/src/database/createConnection.js
+++ packages/server/src/database/createConnection.js
@@ -4,18 +4,6 @@ const createConnection = async () => {
   const firebaseConfig = {
     default: 'DEFAULT',
     apiKey: process.env.FIREBASE_API_KEY,
-    credential: firebase.credential.cert({
-      type: process.env.type,
-      project_id: process.env.project_id,
-      private_key_id: process.env.private_key_id,
-      private_key: process.env.private_key,
-      client_email: process.env.client_email,
-      client_id: process.env.client_id,
-      auth_uri: process.env.auth_uri,
-      token_uri: process.env.token_uri,
-      auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-      client_x509_cert_url: process.env.client_x509_cert_url
-    }),
     databaseURL: process.env.FIREBASE_DATABASE_URL
   }
 
