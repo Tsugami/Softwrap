36b4278dc9cbb0c6bdab214219bb0f7ae4aa3cdd
Yslan Ramos <yslan3000@gmail.com>
2020-12-09 15:18:12 -0300

    feat: using service json

--- packages/server/src/database/createConnection.js
+++ packages/server/src/database/createConnection.js
@@ -1,21 +1,12 @@
 const firebase = require('firebase-admin')
 
 const createConnection = async () => {
+  const serviceAccountKey = require('../../serviceAccountKey.json')
+
   const firebaseConfig = {
     default: 'DEFAULT',
     apiKey: process.env.FIREBASE_API_KEY,
-    // credential: firebase.credential.cert({
-    //   type: process.env.type,
-    //   project_id: process.env.project_id,
-    //   private_key_id: process.env.private_key_id,
-    //   private_key: process.env.private_key,
-    //   client_email: process.env.client_email,
-    //   client_id: process.env.client_id,
-    //   auth_uri: process.env.auth_uri,
-    //   token_uri: process.env.token_uri,
-    //   auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-    //   client_x509_cert_url: process.env.client_x509_cert_url
-    // }),
+    credential: firebase.credential.cert(serviceAccountKey),
     databaseURL: process.env.FIREBASE_DATABASE_URL
   }
 
