139079584bf5fc41a6d1ae8768e49df3143522ad
Yslan Ramos <yslan3000@gmail.com>
2020-12-09 12:35:58 -0300

    feat: remove cors

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3333
 
 const app = express()
 
-app.use(cors({ origin: process.env.FRONTEND_URL || '*' }))
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 app.use(routes)
 app.use(ErrorHandle)
