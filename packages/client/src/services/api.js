0e73accd1f7816d5921bea899ec6404fe8bcfdf6
Yslan Ramos <yslan3000@gmail.com>
2020-12-09 11:26:51 -0300

    fix: api url

--- packages/client/src/services/api.js
+++ packages/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.API_URL || `http://localhost:3333`,
+  baseURL: process.env.API_URL || `https://software-teste.herokuapp.com/`,
 });
 
 export function getUsers(data = {}) {
